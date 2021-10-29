edcad5a471dd80662096d3481f696dc13de85082
davide <davide_lupis@yahoo.it>
2021-10-29 23:49:09 +0200

    Deep Learning Exercise

--- AdvancedProgrammingDeepLearning/DeepLearning/Scripts/PytorchPractice.py
+++ AdvancedProgrammingDeepLearning/DeepLearning/Scripts/PytorchPractice.py
@@ -5,12 +5,17 @@ import matplotlib.pyplot as plt
 ####################################################################
 
 # RVS
-x = tc.tensor([1, 2, 3, 4], dtype=tc.float32)
+x = tc.tensor([ i for i in range(10)], dtype=tc.float32)
 # Weights
 w = tc.tensor(0. , requires_grad=True ,dtype=tc.float32)
 # True model
 noise = tc.normal(size=(100,1) , mean= 0  , std= 1 , dtype=tc.float32)
-y_true = 2 * x
+
+
+def true_model(x):
+ return  abs(np.sin(x))+3
+
+y_true = true_model(x)
 
 # Forward propagation to get y_hat
 def forward(x):
@@ -22,8 +27,8 @@ def loss(y_true, y_hat):
 
 
 # Training
-learning_rate = 0.01
-n_iters = 50
+learning_rate = 0.0001
+n_iters = 2500
 for epoch in range(n_iters):
  # Get prediction
  y_hat = forward(x)
@@ -42,10 +47,10 @@ for epoch in range(n_iters):
  w.grad.zero_()
 
  #verbose every multiple of 10
- if epoch % 2 ==0:
+ if epoch % 10 ==0:
   print(f'Current Iteration: {epoch}, current weight: {w:.3f} , current loss: {L:.5f}')
 
 print('')
 print(f'Prediction after training: f(5) = {forward(5).item():.3f}')
-
+print(f'True value f(5) : {true_model(5):.3f}')
 # We dont need to compute analytical expression for the gradient
